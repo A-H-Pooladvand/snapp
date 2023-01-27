3c01e66e5c6e870458071e45b9e44cfdf7343f26
Amirhossein <a.pooladvand@ipaam.ir>
2023-01-27 12:59:10 +0330

    WIP

--- D/main.go
+++ D/main.go
@@ -6,31 +6,23 @@ import (
 )
 
 func main() {
-	nums := []int{12, 54, 89, 21, 66, 47, 14, 285, 96}
+	initial := []int{12, 54, 89, 21, 66, 47, 14, 285, 96}
 
-	// Create a channel to send numbers through the pipeline
 	pipeline := make(chan int)
 
-	// Start 2 concurrent worker functions
-	for i := 0; i < 2; i++ {
-		go worker(pipeline)
+	go worker(pipeline)
+	go worker(pipeline)
+
+	for _, n := range initial {
+		pipeline <- n
 	}
 
-	// Send the numbers through the pipeline
-	for _, num := range nums {
-		pipeline <- num
-	}
 	close(pipeline)
-
-	// Wait for all workers to finish
-	var input string
-	fmt.Scanln(&input)
 }
 
 func worker(pipeline <-chan int) {
-	for num := range pipeline {
-		// Do the calculation
-		result := math.Pow(float64(num), 2)
+	for n := range pipeline {
+		result := math.Pow(float64(n), 2)
 		fmt.Println(result)
 	}
 }
